7bf88058bcd51342fdd2365160003d6e8f2146ac
Charlie Hsieh <charlieofficial@gmail.com>
2016-08-28 16:05:04 +0800

    add dependent into Model class, to delete article with CASCADE

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  has_many :comments
+  #has_many :comments
+  has_many :comments, dependent: :destroy # delete comments which belongs to the article first, if comments exists
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
